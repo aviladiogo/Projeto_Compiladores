22f6ac73ee62898f3dd11ef86b389fc96ee768f9
nandob1411 <75445194+nandob1411@users.noreply.github.com>
2021-10-08 08:16:03 -0300

    Fernandob

---
+++ Projeto_Compiladores/br/com/compiladores/exceptions/IsiLexicalException.java
@@ -0,0 +1,8 @@
+package br.com.compiladores.exceptions;
+
+public class IsiLexicalException extends RuntimeException{
+    public IsiLexicalException(String msg){
+        super(msg);
+    }
+    
+}
